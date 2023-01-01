f40d16678d91a6f0929f0675e832df88813dd4e7
M254-bto <ngechamike26@gmail.com>
2023-01-01 23:29:07 +0300

    user data processing pipeline

--- data.py
+++ data.py
@@ -1,11 +1,13 @@
 import requests
+import pandas as pd
 
 client_id = '4b7fa2bf3f504c359281219a68c48cab'
 client_secret = '0f33ead49e17443387549b6dbe2fb04c'
 
 
-myToken = 'BQAJV2QXbsoMAOrSnaOElbe4g989-JAJtBEYGl8Cl3QrbEho2oNDoGOp33VS6-BW7CQB0jCT4sUSjFRtoeqirs11VOXSfAqtfz2JgLVtjskvQUdfcLZYdxLLzGVhQPYYntC9UURtsDd8wR9Ch9s1-GO92g3clZSC3UOB5r_OxSLFIsFDKG_F85XJSQFtopoL2GH4B8IKyQ9wEBzH7EREQfZN'
+myToken = 'BQC0rNSshfhTBsPRdFd_uMjiwgMGFK1MYgbvirMM3p5zFxZdorPmMyL-gB9P5F2ehN_ZOTNRBKHVSTR_PD_nW6l_F36hFsZkFssk1cr8TzgtKL8E2_2mWHNyODB4LKdAoJLglc6Rt_EIeZrmREdymnxF47XqjuZWxZf7zLEhNuf5n-TCaASERJXDElcpx-A4AylrLP2Xs6J0QMmqsR15aGviWZS_owIgOyo'
 myUrl = 'https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=50'
-head = {'Authorization': 'Bearer f"{myToken}"'}
+head = {'Authorization': 'Bearer {}'.format(myToken)}
 
 id = requests.get('https://api.spotify.com/v1/me', headers=head)
+print(id.json())
